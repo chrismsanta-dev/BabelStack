58764d41c5177dc316359bbd65f7ca6dad71579b
Chris Santamaria <chrismsanta@gmail.com>
2025-10-07 16:21:13 +1100

    [BAB-9] Completed Basic Header Functionality

--- frontend/src/app/shared/header/header.component.ts
+++ frontend/src/app/shared/header/header.component.ts
@@ -3,10 +3,13 @@ import { User } from '../../core/models/user.model';
 import { Store } from '@ngrx/store';
 import { getUser } from '../../core/store/user/user.selectors';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { logoutUser } from '../../core/store/user/user.actions';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [RouterModule, MatButtonModule, MatMenuModule],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
@@ -14,5 +17,9 @@ export class HeaderComponent {
 
   constructor(private store: Store) {
     this.user = this.store.selectSignal(getUser);
+  }
+
+  protected logout(): void {
+    this.store.dispatch(logoutUser());
   }
 }
